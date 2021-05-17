c5ef932bd316b75b6d6139ccfce7391902cff327
edwintantawi <edwintantawi@gmail.com>
2021-05-17 23:53:03 +0700

    refactor json to js object

---
+++ src/scripts/data/products-data.js
@@ -0,0 +1,32 @@
+const PRODUCTS_DATA = [
+  {
+    product_id: '1',
+    shop_name: 'Initshop',
+    shop_location: 'Medan, Indonesia',
+    product_picture: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    product_name: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    product_price: 109.95,
+    product_star: [0, 0, 0, 12, 82],
+    product_sold: 12,
+    product_stock: 86,
+    product_description:
+      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+    product_review: [
+      {
+        review_name: 'ucup',
+        review_timestamp: '2021-05-15T15:04:16.352Z',
+        review_star: 5,
+        review_content:
+          'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+      },
+      {
+        review_name: 'Cing cing',
+        review_timestamp: '2021-05-15T15:04:16.352Z',
+        review_star: 4,
+        review_content: 'So Love it',
+      },
+    ],
+  },
+];
+
+export default PRODUCTS_DATA;
