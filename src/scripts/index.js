475160a8c7f5577238f838fa59a625aad4addb1a
edwintantawi <edwintantawi@gmail.com>
2021-06-05 16:05:32 +0700

    initial localstorage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,8 @@
+const cartLocalStorage = localStorage.getItem('CART_PRODUCT_ID');
+if (!cartLocalStorage) {
+  localStorage.setItem('CART_PRODUCT_ID', JSON.stringify([]));
+}
+
 const authToggler = document.querySelector('.nav__auth-toggler');
 const navAuth = document.querySelector('.nav__auth-action');
 authToggler.addEventListener('click', () => {
